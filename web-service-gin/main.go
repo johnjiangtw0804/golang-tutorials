e80c503e261d19a7175a872336ee5c2f8aea1e6b
johnjiangtw0804 <johnjiangtw0804@gmail.com>
2024-03-09 22:49:36 -0800

    chores

---
+++ web-service-gin/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// gin.Context is the most important part of Gin. It carries request details,
+// validates and serializes JSON, and more. (Despite the similar name, this is different
+// from Go’s built-in context package.)
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
